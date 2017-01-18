718493b504f743863980e0fc99060075aa5de71d
Kyle <ly.sunsmile@gmail.com>
2017-01-18 16:23:35 -0500

    fix some syntx

--- wechat.py
+++ wechat.py
@@ -38,7 +38,7 @@ class WeChat(object):
 		self.pass_ticket = ''
 		self.base_request = {}
 
-
+		self.set_cookies = 'Set-Cookie'
 		self.deviceId = 'e455251312409684'
 		self.syncheck_url_base = 'https://webpush.wx.qq.com/cgi-bin/mmwebwx-bin/synccheck'
 		self.user_agent =  'Mozilla/5.0 (Windows NT 10; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
@@ -50,9 +50,9 @@ class WeChat(object):
 						'ContentType': 'application/json; charset=UTF-8',}
 
 	def _Cookiesupdata(self,r):
-	    if(set_cookies in list(r.headers.keys())):
+	    if(self.set_cookies in list(r.headers.keys())):
 	        regs=r'(\S+)=(\S+); Domain=(\S+); Path=(\S+); Expires=(.+?GMT),'
-	        cookies_needtoset=re.findall(regs,r.headers[set_cookies])
+	        cookies_needtoset=re.findall(regs,r.headers[self.set_cookies])
 	        for item in cookies_needtoset:
 	            self.jar.set(item[0],item[1],domain=item[2],path=item[3])
 
@@ -60,13 +60,13 @@ class WeChat(object):
 	def get_uuid(self):
 		url_get_uuid = 'https://login.weixin.qq.com/jslogin'
 		param_get_uuid ={
-                 'appid':appid,
+                 'appid':self.appid,
                  'fun':'new',
-                 'lang':lang,
+                 'lang':self.lang,
                  '_':int(time.time()),        
 		}
 		data_uuid=requests.get(url_get_uuid,params=param_get_uuid,cookies=requests.utils.dict_from_cookiejar(self.jar))
-		self._Cookiesupdata(self.jar,data_uuid)
+		self._Cookiesupdata(data_uuid)
 		regx = r'window.QRLogin.code = (\d+); window.QRLogin.uuid = "(\S+?)"'
 		pm = re.search(regx, data_uuid.text)
 		self.code = pm.group(1)
@@ -79,7 +79,7 @@ class WeChat(object):
 		          '_': int(time.time())
 		}
 		data_QR=requests.get(url_get_QR,params=param_get_QR,stream=True,cookies=requests.utils.dict_from_cookiejar(self.jar))
-		self._Cookiesupdata(self.jar,data_QR)
+		self._Cookiesupdata(data_QR)
 		with open(self.path_QR , 'wb') as f:
 		    data_QR.raw.decode_content = True
 		    shutil.copyfileobj(data_QR.raw, f)
@@ -89,26 +89,26 @@ class WeChat(object):
 		url_qury='https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login'
 		qury_param = {
 		              'tip':'0',
-		              'uuid':uuid,
+		              'uuid':self.uuid,
 		              'loginicon':'true',
 		              '_':int(time.time()),
 		}
 		data_qury=requests.get(url_qury,params=qury_param,cookies=requests.utils.dict_from_cookiejar(self.jar))
-		Cookiesupdata(self.jar,data_qury)
+		self._Cookiesupdata(data_qury)
 		pm = re.search(r'window.code=(\d+);', data_qury.text)
 		code = pm.group(1)
 		while code != '200':
 		    data_qury=requests.get(url_qury,params=qury_param,cookies=requests.utils.dict_from_cookiejar(self.jar))
-		    Cookiesupdata(self.jar,data_qury)
+		    self._Cookiesupdata(data_qury)
 		    pm = re.search(r'window.code=(\d+);', data_qury.text)
 		    code = pm.group(1)
 		pm = re.search(r'window.redirect_uri="(\S+?)";', data_qury.text)
 		self.redirect_url = pm.group(1) + "&fun=new&version=v2"
-		self.base_url = redirect_url[:redirect_uri.rfind('/')]
+		self.base_url = self.redirect_url[:self.redirect_url.rfind('/')]
 
 	def get_info(self):
-		data_info=requests.get(self.redirect_uri,cookies=requests.utils.dict_from_cookiejar(self.jar))
-		self._Cookiesupdata(self.jar,data_info)
+		data_info=requests.get(self.redirect_url,cookies=requests.utils.dict_from_cookiejar(self.jar))
+		self._Cookiesupdata(data_info)
 		doc = xml.dom.minidom.parseString(data_info.text)
 		root = doc.documentElement
 		for node in root.childNodes:
@@ -130,10 +130,10 @@ class WeChat(object):
 		}  
 		
 	def init_connect(self):
-		url_init = self.base_uri + '/webwxinit?pass_ticket=%s&r=%s' % (self.pass_ticket,int(time.time()))		
-		init_payload={'BaseRequest' : self.BaseRequest}    		
+		url_init = self.base_url + '/webwxinit?pass_ticket=%s&r=%s' % (self.pass_ticket,int(time.time()))		
+		init_payload={'BaseRequest' : self.base_request}    		
 		data_inital = requests.post(url_init, json=init_payload, headers=self.headers,cookies=requests.utils.dict_from_cookiejar(self.jar))
-		self._Cookiesupdata(self.jar,data_inital)
+		self._Cookiesupdata(data_inital)
 		#data_inital_dic = json.loads(data_inital.text.encode('unicode_escape').decode('string_escape'))
 		data_inital_dic=json.loads(data_inital.content.decode('utf-8'))
 		self.user = data_inital_dic['User']
@@ -145,15 +145,15 @@ class WeChat(object):
 		    contact= init_contaclist[i]
 		    if  contact['VerifyFlag'] & 8 != 0:
 		        self.public_users_list.append(contact)
-		    elif contact['UserName'] in special_users:
+		    elif contact['UserName'] in self.special_users:
 		        self.special_users_list.append(contact)
 		    elif '@@' in contact['UserName']:
-		        self.group_list.append(Contact)
+		        self.group_list.append(contact)
 		    else:
-		        self.contact_list.append(Contact)
+		        self.contact_list.append(contact)
 
 	def status_notify(self):
-		url_statusnotify = self.base_uri+'/webwxstatusnotify?lang=zh_CN&pass_ticket=%s' % (self.pass_ticket)
+		url_statusnotify = self.base_url+'/webwxstatusnotify?lang=zh_CN&pass_ticket=%s' % (self.pass_ticket)
 		statusnotify_load = {
 							'BaseRequest': self.base_request,
 							"Code": 3,
@@ -162,14 +162,14 @@ class WeChat(object):
 							"ClientMsgId": int(time.time())
 		        			}
 		data_statusnotify = requests.post(url_statusnotify, json=statusnotify_load, headers=self.headers,cookies=requests.utils.dict_from_cookiejar(self.jar))
-		self._Cookiesupdata(self.jar,data_statusnotify)	
+		self._Cookiesupdata(data_statusnotify)	
 
-	def get_contact():
-		url_getcontact = self.base_uri + '/webwxgetcontact?lang=%s&pass_ticket=%s&skey=%s&r=%s&seq=0' % (self.lang,self.pass_ticket,self.skey,int(time.time()))
+	def get_contact(self):
+		url_getcontact = self.base_url + '/webwxgetcontact?lang=%s&pass_ticket=%s&skey=%s&r=%s&seq=0' % (self.lang,self.pass_ticket,self.skey,int(time.time()))
 		data_contact = requests.get(url_getcontact,cookies=requests.utils.dict_from_cookiejar(self.jar))
-		Cookiesupdata(self.jar,data_contact)
+		self._Cookiesupdata(data_contact)
 		data_contact_dic=json.loads(data_contact.content.decode('utf-8'))
-		member_count =  data_contact_dic['MemberCount']
+		#member_count =  data_contact_dic['MemberCount']
 		member_list = data_contact_dic['MemberList']
 		for i in range(0,len(member_list),1):
 		    contact= member_list[i]
@@ -179,17 +179,29 @@ class WeChat(object):
 		    else:
 		        self.current_total_contact.add(contact['UserName'])
 		    if  contact['VerifyFlag'] & 8 != 0:
-		        self.public_users_lList.append(contact)
-		    elif contact['UserName'] in SpecialUsers:
+		        self.public_users_list.append(contact)
+		    elif contact['UserName'] in self.special_users_list:
 		        self.special_users_list.append(contact)
 		    elif '@@' in contact['UserName']:
 		        self.group_list.append(contact)
 		    else:
 		        self.contact_list.append(contact)
 
+	def get_batch_contact(self):
+		url_getbatchcontact = self.base_url + '/webwxbatchgetcontact?pass_ticket=%s&skey=%s&r=%s' % (self.pass_ticket,self.skey,int(time.time()))
+		batchcontact_load = {
+		                      'BaseRequest':self.base_request,
+		                      'Count' : len(self.group_list),
+		                      "List": [{"UserName": g['UserName'], "EncryChatRoomId":""} for g in self.group_list]
+		                      }
+		data_batchcontact = requests.post(url_getbatchcontact,json=batchcontact_load,cookies=requests.utils.dict_from_cookiejar(self.jar))
+		self._Cookiesupdata(data_batchcontact)
+		#data_batchcontact_dic=json.loads(data_batchcontact.content.decode('utf-8'))		           
 
 
-	def syn_check(self)
+
+
+	def syn_check(self):
 		url_syncheck = 'https://webpush.wx.qq.com/cgi-bin/mmwebwx-bin/synccheck'
 		syncheck_param = {
         				'r':int(time.time()),
@@ -197,24 +209,40 @@ class WeChat(object):
                   		'uin': self.uin,
                   		'skey' : self.skey,
                   		'deviceid': self.deviceId,
-                  		'synckey' : self.synckey,
+                  		'synckey' : self.sync_key_for_syn,
                   		'_': int(time.time()),
                   		}
-		data_syncheck=requests.get(url_syncheck,params=syncheck_param,cookies=requests.utils.dict_from_cookiejar(jar))
-		self._Cookiesupdata(jar,data_syncheck)
+		data_syncheck=requests.get(url_syncheck,params=syncheck_param,cookies=requests.utils.dict_from_cookiejar(self.jar))
+		self._Cookiesupdata(data_syncheck)
 		print(data_syncheck.text)
 
 
-	def web_wx_sync(self)
+	def web_wx_sync(self):
 		''' get message from web'''
-		url_syn = self.base_uri+'/webwxsync?sid=%s&skey%s&lang=%s&pass_ticket=%s' %(self.sid,skey,self.lang,self.pass_ticket)
-		syn_loard={'BaseRequest' : BaseRequest,
-		          'SyncKey' :  SyncKey,
+		url_syn = self.base_url+'/webwxsync?sid=%s&skey%s&lang=%s&pass_ticket=%s' %(self.sid,self.skey,self.lang,self.pass_ticket)
+		syn_loard={'BaseRequest' : self.base_request,
+		          'SyncKey' :  self.sync_key,
 		          'rr' : '1734374280',                  
 		          }
 		data_syn=requests.post(url_syn, json=syn_loard, headers=self.headers,cookies=requests.utils.dict_from_cookiejar(self.jar))
-		self._Cookiesupdata(self.jar,data_syn)
+		self._Cookiesupdata(data_syn)
 		data_syn_dic=json.loads(data_syn.content.decode('utf-8'))
 		self.sync_key = data_syn_dic['SyncKey']
 		self.sync_key_for_syn='|'.join([str(keyVal['Key'])+'_'+str(keyVal['Val']) for keyVal in self.sync_key['List']])
 
+	def send_message(self,message,to_user_name='filehelper'):
+		url_sendmessage = self.base_url + '/webwxsendmsg?pass_ticket=%s&lang=%s' % (self.pass_ticket,self.lang)
+		client_msg_id = str(int(time.time() * 1000)) + str(random.random())[:5].replace('.', '')
+		sendmessage_load = {
+		                     'BaseRequest': self.base_request,
+		                     'Msg': {
+		                             "Type": 1,
+		                             "Content" : message,
+		                             "FromUserName": self.user['UserName'],
+		                             "ToUserName": to_user_name,
+		                             "LocalID": client_msg_id,
+		                             "ClientMsgId": client_msg_id
+		                             }
+		                    }
+		data_sendmessage=requests.post(url_sendmessage, data=json.dumps(sendmessage_load,ensure_ascii=False).encode("utf-8"), headers=self.headers,cookies=requests.utils.dict_from_cookiejar(self.jar))
+		self._Cookiesupdata(self.jar,data_sendmessage)		                  
